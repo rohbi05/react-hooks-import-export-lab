59b452fe2f762fb3855d6181229267bdb8c23999
shantal rhobi <shantal.rohbi@student.moringaschool.com>
2024-07-30 13:06:15 +0300

    complete lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
+import user from '../data/user'
 
 function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        {user.username} is a Web Developer from {user.city}
       </h1>
     </div>
   );
 }
+
+export default Home
